1cd0c27036614f97d5bf827f3b885a55de2e4f33
ManczakAdrian <manczakadrian@gmail.com>
2022-07-11 01:27:28 +0200

    login and role

--- src/main/java/pl/manczak/springboot2security/WebSecurityConfig.java
+++ src/main/java/pl/manczak/springboot2security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package pl.manczak.springboot2security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -15,9 +16,10 @@ import java.util.Collections;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
@@ -26,12 +28,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 getPasswordEncoder().encode("Jan123"),
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
 
-        User useruser = new User("Karol",
+        User userUser = new User("Karol",
                 getPasswordEncoder().encode("Karol123"),
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
 
 
         auth.inMemoryAuthentication().withUser(userAdmin);
-        auth.inMemoryAuthentication().withUser(useruser);
+        auth.inMemoryAuthentication().withUser(userUser);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/forAdmin").hasRole("ADMIN")
+                .antMatchers("/forUser").hasRole("USER")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/forAll");
+
     }
 }
